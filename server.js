fef798cd749b479a8e7fcdd77f77c52b98afde11
strategygd80 <operations.petbizexperts@gmail.com>
2026-06-19 19:41:15 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,12 @@
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // ------------------------------------------------------------
